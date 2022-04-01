d2a5118cf425be2a44aa51fd5878cdefb3f50b02
Gabriel Henton <hentongabriel@outlook.com>
2022-03-31 18:49:38 -0700

    added frontend UI

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,19 +2,21 @@ const express = require('express')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
+const { protect } = require('../middleware/authMiddleware')
+
 
 // if we really wanted to clean up 4 lines so they are now 2 we could do this...
 // router.route('/').get(getGoals).post(setGoal)
 // router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 
-router.get('/', getGoals) 
+router.get('/', protect, getGoals) 
 
-router.post('/', setGoal)
+router.post('/', protect, setGoal)
 
-router.put('/:id', updateGoal)
+router.put('/:id', protect, updateGoal)
 
-router.delete('/:id', deleteGoal)
+router.delete('/:id', protect, deleteGoal)
 
 
 module.exports = router
