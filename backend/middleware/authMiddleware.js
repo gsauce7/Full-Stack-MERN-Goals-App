735739b92071f20db259151572197046d7afbae1
Gabriel Henton <hentongabriel@outlook.com>
2022-04-01 16:42:45 -0700

    Deploy ready

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,13 +2,17 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
-const protect = asyncHandler(async(req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
     let token
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+    if (
+      req.headers.authorization && req.headers.authorization.startsWith('Bearer')
+    ) {
         try {
           // Get token from header
           token = req.headers.authorization.split(' ')[1]  
+          
+          // Verify token
           const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
           // Get user from the token 
@@ -22,9 +26,9 @@ const protect = asyncHandler(async(req, res, next) => {
         }
     }
 
-    if(!token) {
+    if (!token) {
         res.status(401)
-        throw new Error('Not authorized. No token')
+        throw new Error('Not authorized, no token')
     }
 })
 
